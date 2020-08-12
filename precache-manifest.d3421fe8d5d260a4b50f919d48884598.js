ce6912e6e038c0ca59c197b85a2469d721245ea6
yuxizhe <yuxizhe2008@gmail.com>
2020-08-12 13:11:11 +0000

    Deploying to gh-pages from  @ 5c102ebed0ca61de014c1dd4cbbdacad033bed7c 🚀

---
+++ precache-manifest.d3421fe8d5d260a4b50f919d48884598.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2093c7ca646b8579f18d6971f1164aed",
+    "url": "/crm-maker/index.html"
+  },
+  {
+    "revision": "5406d7f72ca07a57361a",
+    "url": "/crm-maker/static/js/2.09bd3105.chunk.js"
+  },
+  {
+    "revision": "73b207d866c46f1507394f1c54d6ac97",
+    "url": "/crm-maker/static/js/2.09bd3105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8657185ce89cf9e0645b",
+    "url": "/crm-maker/static/js/main.c4030669.chunk.js"
+  },
+  {
+    "revision": "fdc484f4f8ea9167d1f3",
+    "url": "/crm-maker/static/js/runtime-main.c7b1230c.js"
+  }
+]);
